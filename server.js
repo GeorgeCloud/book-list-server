b677a09246288038dceee76db5e66c898a67d355
George <mlgkiirb@gmail.com>
2017-12-12 09:42:33 -0800

    added test path to url

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const CLIENT_URL = process.env.CLIENT_URL;
 
 const client = new pg.Client(process.env.DATABASE_URL);
@@ -17,4 +17,19 @@ app.use(cors());
 
 app.get('/', (req, res) => res.send('Testing 1, 2, 3'));
 
+app.get('/test', (req, res) => res.send('hello world'));
+
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+
+function loadDatabase(){
+  client.query(`
+    CREATE TABLE IF NOT EXIST
+    books (
+    author VARCHAR(25),
+    isbn PRIMARY KEY,
+    image_url VARCHAR(200),
+    description VARCHAR(500)
+    );`
+  )
+}
