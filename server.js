63da576b147a53c6362fad9c25edd6f4282a8dd9
George <mlgkiirb@gmail.com>
2017-12-12 12:37:34 -0800

    connection string added

--- server.js
+++ server.js
@@ -7,9 +7,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const CLIENT_URL = process.env.CLIENT_URL;
+const conString = 'postgres://localhost:5432';
 
 const client = new pg.Client(process.env.DATABASE_URL);
-client.connect();
+client.connect(conString);
 
 client.on('error', err => console.error(err));
 
@@ -17,19 +18,22 @@ app.use(cors());
 
 app.get('/', (req, res) => res.send('Testing 1, 2, 3'));
 
-app.get('/test', (req, res) => res.send('hello world'));
-
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
+// function getBookCount(){
+//   client.query(`
+//     SELECT COUNT(*) FROM books;
+//   `)
+// }
 
-function loadDatabase(){
-  client.query(`
-    CREATE TABLE IF NOT EXIST
-    books (
-    author VARCHAR(25),
-    isbn PRIMARY KEY,
-    image_url VARCHAR(200),
-    description VARCHAR(500)
-    );`
-  )
+// function loadDatabase(){
+//   client.query(`
+//     CREATE TABLE IF NOT EXIST
+//     books (
+//     author VARCHAR(25),
+//     isbn PRIMARY KEY,
+//     image_url VARCHAR(200),
+//     description VARCHAR(500)
+//     );`
+//   )
 }
